2aa614b436bd4cd43d18c091a8f7cd179d4c8211
JuliaWinHau <juliawinsevik@icloud.com>
2025-09-12 12:36:26 +0200

    Removed GBFS

--- plots.py
+++ plots.py
@@ -157,59 +157,59 @@ data = [
                 [3.3166247903554, 3.3166247903554, 4.242640687119285] #time on ground
             ]
         ]
-    ],
-    [ # GBFS
-        [ # map 1
-            [ # ground-to-ground
-                [69.87005768508878, 24.313708498984763, 42.970562748477136], #lengths
-                [6.65685424949238, 6.65685424949238, 7.65685424949238] #time on ground
-            ],
-            [ # air-to-ground
-                [47.60550828100967, 67.60550828100963, 44.334159300226545], #lengths
-                [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
-            ]
-        ],
-        [ # map 2
-            [ # ground-to-ground
-                [67.1920236764956, 18.192023676495612, 41.07106781186546], #lengths
-                [6.65685424949238, 6.65685424949238, 7.65685424949238] #time on ground
-            ],
-            [ # air-to-ground
-                [45.3662673822424, 65.36626738224237, 44.91994573785345], #lengths
-                [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
-            ]
-        ],
-        [ # map 3
-            [ # ground-to-ground
-                [62.60623723886867, 17.36359655174942, 41.170843917394784], #lengths
-                [7.292528739883945, 7.292528739883945, 8.928203230275509] #time on ground
-            ],
-            [ # air-to-ground
-                [45.3662673822424, 65.36626738224237, 44.72719310349883], #lengths
-                [4.878315177510849, 4.878315177510849, 5.196152422706632] #time on ground
-            ]
-        ],
-        [ # map 4
-            [ # ground-to-ground
-                [67.28427124746187, 20.631545744180546, 45.16671517118376], #lengths
-                [6.65685424949238, 6.5604779323150675, 8.610365985079726] #time on ground
-            ],
-            [ # air-to-ground
-                [44.68410462743818, 64.68410462743815, 42.96983379061812], #lengths
-                [4.242640687119286, 4.242640687119286, 4.878315177510849] #time on ground
-            ]
-        ],
-        [ # map 5
-            [ # ground-to-ground
-                [18.07106781186548, 15.292528739883945, 19.656854249492383], #lengths
-                [6.65685424949238, 7.292528739883944, 7.65685424949238] #time on ground
-            ],
-            [ # air-to-ground
-                [26.970562748477146, 24.384776310850242, 18.192023676495612], #lengths
-                [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
-            ]
-        ]
-    ]
+    ]#,
+    #[ # GBFS
+    #    [ # map 1
+    #        [ # ground-to-ground
+    #            [69.87005768508878, 24.313708498984763, 42.970562748477136], #lengths
+    #            [6.65685424949238, 6.65685424949238, 7.65685424949238] #time on ground
+    #        ],
+    #        [ # air-to-ground
+    #            [47.60550828100967, 67.60550828100963, 44.334159300226545], #lengths
+    #            [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
+    #        ]
+    #    ],
+    #    [ # map 2
+    #        [ # ground-to-ground
+    #            [67.1920236764956, 18.192023676495612, 41.07106781186546], #lengths
+    #            [6.65685424949238, 6.65685424949238, 7.65685424949238] #time on ground
+    #        ],
+    #        [ # air-to-ground
+    #            [45.3662673822424, 65.36626738224237, 44.91994573785345], #lengths
+    #            [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
+    #        ]
+    #    ],
+    #    [ # map 3
+    #        [ # ground-to-ground
+    #            [62.60623723886867, 17.36359655174942, 41.170843917394784], #lengths
+    #            [7.292528739883945, 7.292528739883945, 8.928203230275509] #time on ground
+    #        ],
+    #        [ # air-to-ground
+    #            [45.3662673822424, 65.36626738224237, 44.72719310349883], #lengths
+    #            [4.878315177510849, 4.878315177510849, 5.196152422706632] #time on ground
+    #        ]
+    #    ],
+    #    [ # map 4
+    #        [ # ground-to-ground
+    #            [67.28427124746187, 20.631545744180546, 45.16671517118376], #lengths
+    #            [6.65685424949238, 6.5604779323150675, 8.610365985079726] #time on ground
+    #        ],
+    #        [ # air-to-ground
+    #            [44.68410462743818, 64.68410462743815, 42.96983379061812], #lengths
+    #            [4.242640687119286, 4.242640687119286, 4.878315177510849] #time on ground
+    #        ]
+    #    ],
+    #    [ # map 5
+    #        [ # ground-to-ground
+    #            [18.07106781186548, 15.292528739883945, 19.656854249492383], #lengths
+    #            [6.65685424949238, 7.292528739883944, 7.65685424949238] #time on ground
+    #        ],
+    #        [ # air-to-ground
+    #            [26.970562748477146, 24.384776310850242, 18.192023676495612], #lengths
+    #            [4.242640687119286, 4.242640687119286, 4.242640687119286] #time on ground
+    #        ]
+    #    ]
+    #]
 ] # Add other algorithms as needed
 
 # Extract data
@@ -230,29 +230,27 @@ for i, algorithm in enumerate(data):
                 collated_ground[i].append(grounded)
                 #here are your distance near ground numbers
 
-algorithms = ["A*", "Dijkstra", "LazyThetaStar", "GBFS"]
+algorithms = ["A*", "Dijkstra", "LazyThetaStar"]
 
 plt.boxplot(collated_dist, labels=algorithms)
-plt.title("Overall distance of paths by all algorithms")
+plt.title("Distances of paths by all algorithms")
 plt.ylabel("Distance (m)")
 plt.show()
 
 plt.boxplot(collated_ground, labels=algorithms)
-plt.title("Distance near ground by all algorithms")
+plt.title("Distances only near ground by all algorithms")
 plt.ylabel("Distance (m)")
 plt.show()
-
-# TODO remove penguins from code
 
 # extract data
 # we need one list of averages of each algorithm for each value
 
-total_avg = [0, 0, 0, 0] #avg of all runs
-total_avg_g = [0, 0, 0, 0] #avg ground distance of all runs
-ground_avg = [0, 0, 0, 0] #avg all ground-to ground
-ground_avg_g = [0, 0, 0, 0] #etc.
-air_avg = [0, 0, 0, 0]
-air_avg_g = [0, 0, 0, 0]
+total_avg = [0, 0, 0] #avg of all runs
+total_avg_g = [0, 0, 0] #avg ground distance of all runs
+ground_avg = [0, 0, 0] #avg all ground-to ground
+ground_avg_g = [0, 0, 0] #etc.
+air_avg = [0, 0, 0]
+air_avg_g = [0, 0, 0]
 for i, algorithm in enumerate(data):
     for map in algorithm:
         groundlist = map[0]
@@ -269,18 +267,18 @@ for i, algorithm in enumerate(data):
         for g in airlist[1]:
             total_avg_g[i] += g
             air_avg_g[i] += g
-for i, t in enumerate(total_avg):
-    total_avg[i] = t/n
 for i, t in enumerate(total_avg_g):
     total_avg_g[i] = t/n
-for i, t in enumerate(ground_avg):
-    ground_avg[i] = t/gn
+for i, t in enumerate(total_avg):
+    total_avg[i] = (t/n) - total_avg_g[i]
 for i, t in enumerate(ground_avg_g):
     ground_avg_g[i] = t/gn
-for i, t in enumerate(air_avg):
-    air_avg[i] = t/an
+for i, t in enumerate(ground_avg):
+    ground_avg[i] = (t/gn) - ground_avg_g[i]
 for i, t in enumerate(air_avg_g):
     air_avg_g[i] = t/an
+for i, t in enumerate(air_avg):
+    air_avg[i] = (t/an) - air_avg_g[i]
 
 weight_counts = {
     "Near ground": np.array(total_avg_g),
@@ -288,14 +286,14 @@ weight_counts = {
 }
 width = 0.5
 
-bottom = np.zeros(4)
+bottom = np.zeros(3)
 
 for boolean, weight_count in weight_counts.items():
     p = plt.bar(algorithms, weight_count, width, label=boolean, bottom=bottom)
     bottom += weight_count
 
-plt.title("Average lengths of paths")
-plt.legend(loc="upper right")
+plt.title("Average lengths of all paths")
+plt.legend(loc="lower right")
 
 plt.show()
 
@@ -303,8 +301,8 @@ collated_dist_w_0 = []
 collated_ground_w_0 = []
 
 for i, algorithm in enumerate(data):
-    a = [0]
-    b = [0]
+    a = []
+    b = []
     for map in algorithm:
         for type in map:
             for distance in type[0]:
@@ -319,15 +317,17 @@ x = range(0, len(collated_dist_w_0[0]))
 plt.plot(x, collated_dist_w_0[0], "o", label="AStar")
 plt.plot(x, collated_dist_w_0[1], "o", label="Dijkstra")
 plt.plot(x, collated_dist_w_0[2], "o", label="Lazy ThetaStar")
-plt.plot(x, collated_dist_w_0[3], "o", label="GBFS")
 plt.title("Difference in path length across all runs")
-plt.legend(title="Algorithms:", loc="lower left")
+plt.legend(title="Algorithms:", loc="upper right", fontsize="x-small")
+plt.ylabel("Distance (m)")
+plt.xlabel("Runs")
 plt.show()
 
 plt.plot(x, collated_ground_w_0[0], "o", label="AStar")
 plt.plot(x, collated_ground_w_0[1], "o", label="Dijkstra")
 plt.plot(x, collated_ground_w_0[2], "o", label="Lazy ThetaStar")
-plt.plot(x, collated_ground_w_0[3], "o", label="GBFS")
 plt.title("Difference in grounded path length across all runs")
-plt.legend(title="Algorithms:", loc="lower left")
+plt.legend(title="Algorithms:", loc="upper right", fontsize="x-small")
+plt.ylabel("Distance (m)")
+plt.xlabel("Runs")
 plt.show()
